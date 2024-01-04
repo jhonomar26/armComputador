a3b5e680ac1eb75ea905fb2ce4812c579c11098e
Your Name <you@example.com>
2024-01-04 17:02:31 -0500

    andres

--- armacomputador/settings.py
+++ armacomputador/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "armcomputador",
         "USER": "postgres",
-        "PASSWORD": "guerrero191102",
+        "PASSWORD": "postgre1",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
